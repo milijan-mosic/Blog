910011223417ad373eda912db260ebd61327d22c
milijan-mosic <milijan.mosic@gmail.com>
2024-12-22 19:30:46 +0100

    Updated config file

--- src/config.py
+++ src/config.py
@@ -2,10 +2,15 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///site.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = os.environ.get('EMAIL_USER', 'your-email@gmail.com')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS', 'your-email-password')
+
+    LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'DEBUG')
